26826f3ab30c29968af5202565377bf0ef7ab77c
0xc14m1z <0xc14m1z@gmail.com>
2019-01-16 19:28:43 +0100

    Add other tests for #validate method

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -1,10 +1,49 @@
 import { expect } from 'chai'
 
+import validate from 'src/validate'
+
 const testValidate = () => {
-  it('should throw if schema is undefined')
-  it('should throw if schema is not given at all')
-  it('should pass if schema is valid')
-  it('should pass if schema has no errors')
+
+  it('should throw if schema is undefined', () => {
+    const schema = undefined,
+          result = () => validate(schema)
+    expect(result).to.throw()
+  })
+
+  it('should throw if schema is not given at all', () => {
+    const result = () => validate()
+    expect(result).to.throw()
+  })
+
+  it('should throw if the target is undefined', () => {
+    const schema = {},
+          target = undefined
+          result = () => validate(schema)(target)
+    expect(result).to.throw()
+  })
+
+  it('should throw if the target is not given at all', () => {
+    const schema = {},
+          result = () => validate(schema)()
+    expect(result).to.throw()
+  })
+
+  it('should pass if validation result has the right shape')
+
+  it('should pass if schema is valid', () => {
+    const schema = {},
+          target = {},
+          result = validate(schema)(target)
+    expect(result.isValid).to.be.true
+  })
+
+  it('should pass if schema has no errors', () => {
+    const schema = {},
+          target = {},
+          result = validate(schema)(target)
+    expect(result.errors).to.be.an('array').that.is.empty
+  })
+
 }
 
 export default testValidate
