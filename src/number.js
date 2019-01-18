fa5c34ccbc069d8031ac5fee1bd017415851b501
0xc14m1z <0xc14m1z@gmail.com>
2019-01-18 13:33:45 +0100

    Remove useless import

--- src/number.js
+++ src/number.js
@@ -1,6 +1,5 @@
-import optional from './optional'
-
 const number = value =>
-  typeof value === 'number' && !Number.isNaN(value) || value instanceof Number
+  typeof value === 'number' && !Number.isNaN(value)
+    || value instanceof Number
 
 export default number
