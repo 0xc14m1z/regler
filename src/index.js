ffaebe5c637d85dbe2aee96bde1d4bd0a0faa5a4
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-19 15:15:35 +0100

    Export all base validators

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import validate from './validate'
 
+import array from './array'
+import boolean from './boolean'
+import fn from './fn'
+import number from './number'
+import object from './object'
+import optional from './optional'
+import required from './required'
+import string from './string'
+
 export default {
-  validate
+  validate,
+
+  array,
+  boolean,
+  fn,
+  number,
+  object,
+  optional,
+  required,
+  string
 }
