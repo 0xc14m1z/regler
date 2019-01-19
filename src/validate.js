a8896e86e32b84138a5ca17bb266f1d9f4859279
Luca Ramundo <0xc14m1z@gmail.com>
2019-01-19 20:26:41 +0100

    Refactor #validate method

--- src/validate.js
+++ src/validate.js
@@ -1,6 +1,29 @@
-const throwIf = condition => {
-  if ( condition ) throw new Error()
-}
+import throwIf from './throwIf'
+
+const DEFAULT_RESULT = { isValid: false, errors: {} }
+const INITIAL_RESULT = { isValid: true, errors: {} }
+
+const empty = object =>
+  Object.keys(object).length === 0
+
+const addError = (errors, error) =>
+  (errors || []).concat(error)
+
+const addFailure = (errors, property, validatorName) => ({
+  isValid: false,
+  errors: { ...errors, [property]: addError(errors[property], validatorName) }
+})
+
+const performValidator = (status, validator, property, value) =>
+  validator(value)
+    ? status
+    : addFailure(status.errors, property, validator.name)
+
+const checkKey = target => (result, [property, validator]) =>
+  performValidator(result, validator, property, target[property])
+
+const checkKeys = (schema, target) =>
+  Object.entries(schema).reduce(checkKey(target), INITIAL_RESULT)
 
 const validate = schema => {
   throwIf(schema === undefined)
@@ -8,28 +31,8 @@ const validate = schema => {
   return target => {
     throwIf(target === undefined)
 
-    return (
-      Object.keys(target).length === 0
-        ? { isValid: false, errors: {} }
-        : Object.entries(schema)
-                 .reduce(
-                   ({ isValid, errors }, [property, validator]) => {
-                     return validator(target[property])
-                              ? { isValid, errors }
-                              : {
-                                isValid: false,
-                                errors: {
-                                  ...errors,
-                                  [property]: [
-                                    ...(errors[property] || []),
-                                    validator.name
-                                  ]
-                                }
-                              }
-                   },
-                   { isValid: true, errors: {} }
-                 )
-    )
+    if ( empty(target) ) return DEFAULT_RESULT
+    return checkKeys(schema, target)
   }
 }
 
