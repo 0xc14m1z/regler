233ea9e1b7e79dfdc3e613285f47fa7b575196dc
0xc14m1z <0xc14m1z@gmail.com>
2019-02-18 13:03:29 +0100

    Refactor #throwIf helper to lower cognitive complexity

--- src/helpers/throwIf.js
+++ src/helpers/throwIf.js
@@ -1,24 +1,23 @@
 function throwIf(condition, Class, message) {
-  if ( condition === true ) {
+  if ( condition !== true ) return
 
-    // if the message is given, the Class must be provided.
-    // if it's not, it would throw anyway for a non-callable object exception
-    if ( message )
-      throw new Class(message)
+  // if the message is given, the Class must be provided.
+  // if it's not, it would throw anyway for a non-callable object exception
+  if ( message )
+    throw new Class(message)
 
-    // if the second parameter is a string,
-    // then it's the custom message for a generic Error
-    if ( typeof Class === 'string' )
-      throw new Error(Class)
+  // if the second parameter is a string,
+  // then it's the custom message for a generic Error
+  if ( typeof Class === 'string' )
+    throw new Error(Class)
 
-    // if the second parameter is a function,
-    // then it's the error class to throw
-    if ( typeof Class === 'function' )
-      throw new Class()
+  // if the second parameter is a function,
+  // then it's the error class to throw
+  if ( typeof Class === 'function' )
+    throw new Class()
 
-    // if nothing is given, a generic Error is thrown
-    throw new Error()
-  }
+  // if nothing is given, a generic Error is thrown
+  throw new Error()
 }
 
 export default throwIf
