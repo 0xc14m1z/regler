5350e56c8a6c046f88a4eb49cf42f2880e9d3ec3
0xc14m1z <0xc14m1z@gmail.com>
2019-06-11 16:06:41 +0200

    Export shape and of subvalidators

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import boolean from './boolean'
 import number from './number'
 import string from './string'
 import fn from './function'
+
 import array from './array'
+import of from './array/of'
+
 import object from './object'
+import shape from './object/shape'
+
 import instanceOf from './instanceOf'
 import oneOf from './oneOf'
 
@@ -15,8 +20,13 @@ R.use('boolean', boolean)
 R.use('number', number)
 R.use('string', string)
 R.use('function', fn)
+
 R.use('array', array)
+array.chain('of', of)
+
 R.use('object', object)
+object.chain('shape', shape)
+
 R.use('instanceOf', instanceOf)
 R.use('oneOf', oneOf)
 
